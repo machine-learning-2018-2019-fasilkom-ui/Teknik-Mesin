cce3fc6232fbb67c6e71aa06d5a37a5fe602bc6a
lowell <lowell.sianipar@gmail.com>
2019-04-18 23:33:38 +0700

    Progress 2

--- progress/test.py
+++ progress/test.py
@@ -3,7 +3,7 @@ import time
 from learn.classifier import DT, KNN, RandomForest
 from learn.decomposition import PCA
 from learn.metrics import confusion_matrix, accuracy
-from sklearn.model_selection import train_test_split
+from learn.model_selection import train_test_split
 
 df = pd.read_csv('data\genres.csv')
 
@@ -12,11 +12,6 @@ X = df.drop(columns=col_label)
 y = df[col_label]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-
-# pca = PCA(12)
-# X_train = pca.fit_transform(X_train)
-# X_test = pca.transform(X_test)
-
 
 algos = {
     'knn' : KNN(4),
@@ -29,20 +24,20 @@ clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 print(accuracy(y_test, y_pred))
 print(confusion_matrix(y_test, y_pred))
-
-print("-"*30)
-
-clf = algos['dt']
-
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-print(accuracy(y_test, y_pred))
-print(confusion_matrix(y_test, y_pred))
-
-print("-"*30)
-
-clf = algos['rf']
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-print(accuracy(y_test, y_pred))
-print(confusion_matrix(y_test, y_pred))
+#
+# print("-"*30)
+#
+# clf = algos['dt']
+#
+# clf.fit(X_train, y_train)
+# y_pred = clf.predict(X_test)
+# print(accuracy(y_test, y_pred))
+# print(confusion_matrix(y_test, y_pred))
+#
+# print("-"*30)
+#
+# clf = algos['rf']
+# clf.fit(X_train, y_train)
+# y_pred = clf.predict(X_test)
+# print(accuracy(y_test, y_pred))
+# print(confusion_matrix(y_test, y_pred))
